88c2e585cf566b5e1d5dd9ffa2858ec2a769acac
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-17 20:14:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}

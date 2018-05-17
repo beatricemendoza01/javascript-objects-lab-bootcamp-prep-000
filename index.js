f3b01c25c0d75c529d1d8c847f823665dd8afce6
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-17 20:09:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const recipes = {
   lasagna: ['tomato sauce', 'meat', 'cheese', 'pasta'],
   'peanut butter jelly sandwich': ['bread', 'peanut butter', 'jelly'],
-  
+  pizza: ['dough', 'sauce', 'cheese', 'peperoni']
 }
